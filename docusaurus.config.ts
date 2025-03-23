a7dc294aa97f8786a836832b2dce9d76e00c89d6
Tarek AIT BAHA <44382088+TarekAi@users.noreply.github.com>
2025-03-23 01:28:05 +0000

    Deployement

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,16 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://opentutorai.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/open-tutor-ai-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'tarekai', // Usually your GitHub org/user name.
+  projectName: 'open-tutor-ai-docs ', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
