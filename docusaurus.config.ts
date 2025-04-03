b8165566dbb69d21d936c380e9fdc68fd21de0ce
tarekai <tr.aitbaha@gmail.com>
2025-04-03 20:01:42 +0000

    ghpage config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://tarekai.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/open-tutor-ai-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
